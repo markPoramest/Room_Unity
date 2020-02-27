9a739e87bf408c3c3beec9f53da8c8217bf52e85
markza1998 <2411kjn@gmail.com>
2020-02-27 10:41:37 +0700

    Add design file

---
+++ Build/UnityLoader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:a26edeb0943a566523b83df9f6d72578ebb96a7c238e9c611282ba161a7c6dce
+size 157158
---
+++ TemplateData/UnityProgress.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1e0ca5d8ed8f74e81006484e31831e931ff95271c8b1dadad2f1e17032d4f698
+size 1209
